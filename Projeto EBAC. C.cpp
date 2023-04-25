8dcd3826099421c49e241995ba31e24c3b12f848
luizWolf18 <131825463+luizWolf18@users.noreply.github.com>
2023-04-26 00:00:11 +0100

    Opção de saida
    Incluso no sistema opção de saida para o usuario.

--- Projeto EBAC. C.cpp
+++ Projeto EBAC. C.cpp
@@ -128,7 +128,8 @@ int main()
 	 printf("Escolha a opção desejada do menu:\n");
 	 printf("\t1- Registrar nomes\n");
 	 printf("\t2- Consultar nomes\n");
-	 printf("\t3- Deletar nomes\n\n");//fim do menu
+	 printf("\t3- Deletar nomes\n\n");
+	 printf("\t4- Sair do Sistema\n\n");
 	 
 	 scanf("%d", &opcao);//armazenando a escolha do usuario
 	 
@@ -147,6 +148,11 @@ int main()
 		
 		   case 3:
 		   deletar();
+		   break;
+		   
+		   case 4:
+		   printf("Obrigado por utilizar o sistema!\n");
+		   return 0;
 		   break;
 			
 		   default:
